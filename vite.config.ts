36b5fa1f96e1095c55962aa8b7894a7e98d6fbe7
JonaCardozoo <futjona456@gmail.com>
2024-11-17 00:14:57 -0300

    api resuelto

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': {
+        target: 'https://apinode-production-5616.up.railway.app',  // Aquí va la URL de tu API en Railway
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')  // Reescribe /api a la raíz de tu API
+      },
     },
   },
 });
